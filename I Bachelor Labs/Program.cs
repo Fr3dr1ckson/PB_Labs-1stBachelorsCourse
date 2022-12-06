28a4806e100cedca0bf4cfd24dff1904e79d52d0
Fr3dr1ckson <onargo@gmail.com>
2022-12-06 14:30:30 +0200

    Initial commit

---
+++ I Bachelor Labs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace I_Bachelor_Labs;
+
+class RunField
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+    }
+}
+   
