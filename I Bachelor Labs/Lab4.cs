30b0e29dc962b839de9e94f50cd8887607331896
Fr3dr1ckson <onargo@gmail.com>
2022-12-06 14:47:01 +0200

    Added Lab4

---
+++ I Bachelor Labs/Lab4.cs
@@ -0,0 +1,45 @@
+﻿namespace I_Bachelor_Labs;
+
+public class Lab4
+{
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;        
+    }
+
+    public static void Mersenne(int n)
+    {
+        if (n < 0)
+        {
+            Console.WriteLine("Введене число не є натуральним");
+        }
+        else if (n == 0 || n == 1 || n == 2)
+        {
+            Console.WriteLine($"Нема жодного простого числа Мерсенна, що менші за {n}");
+        }
+        else
+        {
+            for (int i = 1; Math.Pow(2, i) - 1 <= n; i++)
+            {
+                if (IsPrime(i))
+                {
+                    if (Math.Pow(2, i) - 1 < n)
+                    {
+                        Console.WriteLine($"Число Мерсенна, що менше за {n}: {Math.Pow(2, i) - 1}");
+                    }
+                }
+            }
+        }
+    }
+}
--- I Bachelor Labs/Program.cs
+++ I Bachelor Labs/Program.cs
@@ -11,7 +11,8 @@ class RunField
             Console.OutputEncoding = Encoding.UTF8;
             Console.Write( "1: Лабораторна 3, завдання 1 - Обчислення функції" +'\n' +
                               "2: Лабораторна 3, завдання 2 - Обчислення периметра і площі" +'\n' +
-                              "3: Лабораторна 3, завдання 3 - Обчислення функції, з декількома інпутами");
+                              "3: Лабораторна 3, завдання 3 - Обчислення функції, з декількома інпутами" + '\n' +
+                              "4: Лабораторна 4, завдання 1 - Виведення чисел Мерсена, що менші за інпут");
             ConsoleKey keypressed1;
             ConsoleKeyInfo keyInfok = Console.ReadKey(true);
             if (Int32.TryParse(keyInfok.KeyChar.ToString(), out _))
@@ -72,11 +73,36 @@ class RunField
                         Console.WriteLine("Результат функції: " + Lab3.Task3(x, b, c));
                         break;
                     }
+                    
                     catch (FormatException)
                     {
                         Console.WriteLine("Невірний інпут");
                         throw;
                     }
+                case ConsoleKey.D4:
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.Write("Введіть n: ");
+                        int input = int.Parse(Console.ReadLine());
+                        Lab4.Mersenne(input);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Невірний інпут");
+                        throw;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Число завелике, можливі значення від 1 до 2147483647");
+                        throw;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                    break;
             }
         } while (Console.ReadLine()?.ToLower() != "вийти");
     }
